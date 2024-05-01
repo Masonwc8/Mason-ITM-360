09403f05f05a7fd0c2259336f7154acf0d4a11ef
Masonwc8 <156938195+Masonwc8@users.noreply.github.com>
2024-05-01 10:50:58 -0400

    Delete Project2 directory

--- Project2/proj2-1.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from password_manager_blueprint import password_manager_bp
-
-app = Flask(__name__)
-app.register_blueprint(password_manager_bp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
